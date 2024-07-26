51839520e385e9be9a1eb6b27bffe799084c5d86
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-07-26 17:55:45 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,15 @@
   use App\Http\Controllers\Api\AuthController;
   use Illuminate\Support\Facades\Route;
   
-  Route::post('register', [AuthController::class, 'register']);
-  Route::post('login', [AuthController::class, 'login']);
-  
-  Route::middleware('auth:api')->group(function () {
-    Route::get('user', [AuthController::class, 'user']);
-    Route::post('logout', [AuthController::class, 'logout']);
+  Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    
+    Route::middleware('auth:api')->group(function () {
+      Route::get('user', [AuthController::class, 'user']);
+      Route::post('logout', [AuthController::class, 'logout']);
+    });
+    
+    Route::get('/', fn() => response()->json(['message' => 'Hello World!']));
   });
-  
-  Route::get('/', function () {
-    return response()->json(['message' => 'Hello World!']);
-  });
+ 
